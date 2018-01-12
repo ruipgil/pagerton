7f50ee530691af2fdecede2dc589b2f890ae8128
Rui Gil <ruipgil@users.noreply.github.com>
2018-01-12 18:32:37 +0000

    Remove hasNext option on when calling paginate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import throttler from 'f-throttler'
 
-const generatePromises = (pages, fn) => {
+// Generates an array for the same function
+const generateFunctions = (pages, fn) => {
   let arr = []
   for (let i = 0; i < pages; i++) {
     arr.push(fn)
@@ -8,16 +9,43 @@ const generatePromises = (pages, fn) => {
   return arr
 }
 
+// Flattens an array
 const flatten = (arr) =>
   arr.reduce((prev, current) =>
     prev.concat(current), [])
 
-const defaultJitter = (i, count) => Math.random() * (i / count) * 100
+// Default jitter function
+const defaultJitter = (i, count) => Math.random() * 100
 
+/**
+ * Efficiently paginates a resource based on a paginator function
+ *
+ * @param  {!function(Number=):Promise} paginator Paginator function
+ *  the promise should return an object in the form of:
+ *  { results: Array, perPage: Number, total: Number }
+ *  where:
+ *  - total is total number of resources
+ *  - perPage is the number of resources per page
+ *  - results is the result gathered from a page
+ *
+ * @param  {Object} options Options object to pass to  the `throttler`
+ *  package. In the form:
+ *  {
+ *    jitter: Function(Number=, Number=),
+ *    interval: Number,
+ *    concurrency: Number
+ *  }
+ *  where:
+ *  - jitter: is a function that returns a number, it will be
+ *    used to delay the execution of the function. The default
+ *    function returns a random number between 0 and 100 (ms).
+ *  - interval: minimum number of milliseconds between requests
+ *  - concurrency: number of requests made concurrently
+ */
 const paginate = async (paginator, options = {}) => {
-  const { results, perPage = 1, hasNext, total } = await paginator(0)
+  const { results, perPage = 1, total } = await paginator(0)
 
-  if (!hasNext) {
+  if (results.length >= total) {
     return results
   }
 
@@ -27,9 +55,7 @@ const paginate = async (paginator, options = {}) => {
     concurrency = 2
   } = options
   const pages = Math.ceil(total / perPage)
-  const promises = generatePromises(pages, (i) => {
-    return paginator(i + 1)
-  })
+  const promises = generateFunctions(pages, (i) => paginator(i + 1))
 
   const allResults = await throttler(promises, {
     jitter,
